15f7bdc7a154365e8ce74494acbd3e542c895dad
OnanThairu <thairuonan@gmail.com>
2022-09-10 16:09:58 +0300

    Add users controller and use it in config.ru

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,9 @@ class UsersController < ApplicationController
   # Responsible for anything involving our user
 
   # sign up
-  get "/signup" do
-  end
 
   post "/signup" do
+    binding.pry
     
   end
 
