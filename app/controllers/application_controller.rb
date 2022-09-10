16701da14ad5f0ab774aa09d3b4fda7826592ae0
OnanThairu <thairuonan@gmail.com>
2022-09-10 14:26:32 +0300

    Enable sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
   
   # Add your routes here
   get "/blockers-list" do
