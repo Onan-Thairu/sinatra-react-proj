795d321c707c1d3962cbffa3dfb70bc565956989
OnanThairu <thairuonan@gmail.com>
2022-09-10 17:26:11 +0300

    Signup logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,20 @@ class UsersController < ApplicationController
   # sign up
 
   post "/signup" do
-    binding.pry
+    # Receive data from the form inside of params hash.
+    # Create a new author object with the data.
+    user = User.new(params)
+
+    # Validate our user.
+    # Persist the new object.
+    if user.username.blank? || user.email.blank? || user.password.blank? do
+      redirect "/signup"
+    else
+      user.save
+      redirect "/blockers-list"
+    end
+    
+    # binding.pry
     
   end
 
